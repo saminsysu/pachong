4772d04f33dad11761ce991453010c7f9f68632d
saminsysu <674733146@qq.com>
2016-11-03 16:15:46 +0800

    modify 2.py

--- 2.py
+++ 2.py
@@ -14,8 +14,36 @@ URL='https://movie.douban.com/top250'
 def download_page(url):
 	return requests.get(url).content
 
+def parse_html(html):
+	s=BeautifulSoup(html,'html.parser')
+	o=s.find("ol",attrs={'class':"grid_view"})
+	film_list=[]
+	for l in o.find_all("li"):
+		Num=l.find("em",attrs={'class':''}).get_text()
+		hd=l.find('div','hd')
+		titles=hd.find_all("span","title")
+		other_title=hd.find("span","other").get_text()
+		Title=""
+		for i in range(len(titles)):
+			Title+=titles[i].get_text()
+		Title+=other_title
+		quote=l.find('span','inq')
+		Quote=""
+		if quote:
+			Quote=quote.get_text()
+		film_list.append({'num':Num,'title':Title,'quote':Quote})
+	next_page=s.find("span","next").a
+	if next_page:
+		return film_list,URL+next_page['href']
+	return film_list,None
+
 def main():
-	print download_page(URL)
+	with open('films.txt','w') as fp:
+		url=URL
+		while url:
+			filmList,url=parse_html(download_page(url))
+			for film in filmList:
+				fp.write(film['num']+'.'+film['title']+'\n'+film['quote']+'\n')
 
 if __name__=='__main__':
 	main()
